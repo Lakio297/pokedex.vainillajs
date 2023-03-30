df28bda553081ef3fc82de57024b394394e0e342
Gerardo Gastelum <gegage98@gmai.com>
2023-03-29 18:02:20 -0700

    Developing API

--- main.js
+++ main.js
@@ -4,19 +4,34 @@ const searchBar = document.querySelector('.app__search')
 const appContainer = document.querySelector('.app__container')
 /* ===== VARIABLES ===== */
 const URL = 'https://pokeapi.co/api/v2/pokemon'
-const nextUrl = ''
-const previousUrl = ''
+let nextUrl = ''
+let previousUrl = ''
 /* ===== FUNCTIONS ===== */
-function getData () {
+async function getData () {
+  // window.fetch(URL)
+  // .then((res) => res.json())
+  // .then((data) => {
+  //   console.log(data);
 
-  window.fetch(URL).then((res) => {
-    res.json()
-  }).then((data) => {
-    console.log(data);
-  })
+  //   nextUrl = data.next
+  //   previousUrl = data.previous
 
+  //   printPokemons(data.results)
+  // })
+
+  const res = await window.fetch(URL)
+  const {next, previous, result} = await res.json()
+
+  nextUrl = next
+  previousUrl = previous
+
+  
 }
 
 getData()
+
+function printPokemons (data) {
+  console.log(data);
+}
 /* ===== LISTENERS ===== */
 

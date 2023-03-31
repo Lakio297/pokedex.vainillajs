ccd9b05ad577debe0247981290353b85e101ebbb
Gerardo Gastelum <gegage98@gmai.com>
2023-03-31 15:36:25 -0700

    Final details

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /* ===== GET HTML ELEMENTS ===== */
-const app = document.querySelector('.app')
-const searchBar = document.querySelector('.app__search')
-const appContainer = document.querySelector('.app__container')
+const app = document.querySelector(".app");
+const searchBar = document.querySelector(".app__search");
+const appContainer = document.querySelector(".app__container");
 /* ===== VARIABLES ===== */
-const URL = 'https://pokeapi.co/api/v2/pokemon'
-let nextUrl = ''
-let previousUrl = ''
+const URL = "https://pokeapi.co/api/v2/pokemon";
+let nextUrl = "";
+let previousUrl = "";
 /* ===== FUNCTIONS ===== */
-async function getData (url) {
+async function getData(url) {
   // window.fetch(URL)
   // .then((res) => res.json())
   // .then((data) => {
@@ -19,67 +19,70 @@ async function getData (url) {
   //   printPokemons(data.results)
   // })
 
-  const res = await window.fetch(url)
-  const {next, previous, results } = await res.json()
+  const res = await window.fetch(url);
+  const { next, previous, results } = await res.json();
 
-  nextUrl = next
-  previousUrl = previous
+  nextUrl = next;
+  previousUrl = previous;
 
-  printPokemons(results)
+  printPokemons(results);
 }
 
-
-async function printPokemons (data) {
-  let html = ''
+async function printPokemons(data) {
+  let html = "";
 
   for (const { url } of data) {
-    const res = await window.fetch(url)
-    const { name, sprites } = await res.json()
+    const res = await window.fetch(url);
+    const { name, sprites } = await res.json();
 
     html += `
     <div class="app__item">
-    <img class="app__item--img" src="${sprites.other['official-artwork'].front_default}" alt="${name}">
+    <img class="app__item--img" src="${sprites.other["official-artwork"].front_default}" alt="${name}">
     <h2 class="app__item--name">${name}</h2>
     </div>
-    `
+    `;
   }
 
-  appContainer.innerHTML = html
+  appContainer.innerHTML = html;
 }
 
-const btnNext = () => { nextUrl ? getData(nextUrl) : window.alert('Nada que mostrar')}
-const btnPrev = () => { previousUrl ? getData(previousUrl) : window.alert('Nada que mostrar')}
+const btnNext = () => {
+  nextUrl ? getData(nextUrl) : window.alert("Nada que mostrar");
+};
+const btnPrev = () => {
+  previousUrl ? getData(previousUrl) : window.alert("Nada que mostrar");
+};
 
-getData(URL)
+getData(URL);
 
-async function printPokemon ({ value }) {
-  let html = ''
+async function printPokemon({ value }) {
+  let html = "";
 
-  const res = await window.fetch(URL + `/${value}`)
-  const { name, sprites } = await res.json()
+  const res = await window.fetch(URL + `/${value}`);
+  const { name, sprites } = await res.json();
 
   html += `
   <div class="app__item">
-  <img class="app__item--img" src="${sprites.other['official-artwork'].front_default}" alt="${name}">
+  <img class="app__item--img" src="${sprites.other["official-artwork"].front_default}" alt="${name}">
   <h2 class="app__item--name">${name}</h2>
   </div>
-  `
-  return html
+  `;
+  return html;
 }
 /* ===== LISTENERS ===== */
 
-app.addEventListener('click', ({target}) => {
-  if (target.classList.contains('button--prev')) btnPrev()
-  if (target.classList.contains('button--next')) btnNext()
-  if (target.classList.contains('button--initial')) getData(URL)
-})
-  
-searchBar.addEventListener('change', async ({target}) => {
-  if (target.value !== '') {
-    const html = await printPokemon(target)
+app.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("button--prev")) btnPrev();
+  if (target.classList.contains("button--next")) btnNext();
+  if (target.classList.contains("button--initial")) getData(URL);
+});
 
-    appContainer.innerHTML = html
+searchBar.addEventListener("change", async ({ target }) => {
+  if (target.value !== "") {
+    const html = await printPokemon(target);
+
+    appContainer.innerHTML = html;
   } else {
-    getData(URL)
+    getData(URL);
   }
-})
+});
